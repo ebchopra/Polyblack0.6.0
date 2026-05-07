cb327642ae723c9d26788d15417a7f1d261f19ae
ebchopra <114200182+ebchopra@users.noreply.github.com>
2026-05-06 20:47:29 -0700

    Add files via upload

--- main.bundle.js
+++ main.bundle.js
@@ -36042,10 +36042,10 @@
                             e.shininess.value = Math.max(t.shininess, 1e-4)
                         }(e, a)) : a.isMeshStandardMaterial ? (r(e, a),
                         function(e, t) {
-                            e.metalness.value = t.metalness,
+                            e.metalness.value = 1.0,
                             t.metalnessMap && (e.metalnessMap.value = t.metalnessMap,
                             n(t.metalnessMap, e.metalnessMapTransform));
-                            e.roughness.value = t.roughness,
+                            e.roughness.value = 0.1,
                             t.roughnessMap && (e.roughnessMap.value = t.roughnessMap,
                             n(t.roughnessMap, e.roughnessMapTransform));
                             t.envMap && (e.envMapIntensity.value = t.envMapIntensity)
